08dbca1b8a3f99ef994c955f391cdb832a769bc2
Julio <lamajulio17@gmail.com>
2020-04-02 19:35:08 -0500

    Controller

--- src/main/java/com/website/portfolio/controller/GitHubController.java
+++ src/main/java/com/website/portfolio/controller/GitHubController.java
@@ -2,12 +2,15 @@ package com.website.portfolio.controller;
 
 import com.website.portfolio.client.model.Repository;
 import com.website.portfolio.service.GitHubService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
+@CrossOrigin(origins = "*", methods ={RequestMethod.GET}, allowedHeaders = {"*"})
 @RestController
 public class GitHubController {
 
